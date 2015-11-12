55e22b9b58924a42924ef5977b934ace1e2512bc
Nylon <diego.gomes87@gmail.com>
2015-11-12 15:08:54 -0200

    A musica e o layout são as melhores coisas de App

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet var toolbar: UIToolbar!
+    @IBOutlet var playItem: UIBarButtonItem!
+    @IBOutlet var stopitem: UIBarButtonItem!
+    
+    var audio = AVAudioPlayer()
+    var audioPath = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Tacica",ofType: "mp3")!)
+    
+    //var do Button play
+    
+    var playMu = UIBarButtonItem()
+    var stopMu: Bool = false
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        do {
+            audio = try AVAudioPlayer(contentsOfURL: audioPath)
+        }catch{
+            print("Audio deletado ou nome do arquivo errado !")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +39,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func play(sender: AnyObject) {
+        
+        var item = toolbar.items!
+        
+        if stopMu == false {
+            playMu = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Pause, target: self, action: "PlayPause:")
+            item[0] = stopitem
+            toolbar.setItems(item, animated: true)
+            
+            audio.play()
+            stopMu = true
+            
+        }else{
+        
+            var item = toolbar.items!
+            playMu = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Pause, target: self, action: "PlayPause:")
+            item[0] = playMu
+            toolbar.setItems(item, animated: true)
+            
+            audio.pause()
+            stopMu = false
+        }
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+    }
 
 }
 
